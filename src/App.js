0942a96fb90e05d0dfd83f89deaf78f44a862e1b
MafiaDon2011 <wajidhussain1@live.com>
2023-01-02 14:44:46 +0000

    Created two variables that would fetch response data for next and previous page

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import axios from "axios";
 function App() {
   const [pokemon, setPokemon] = useState([])
   const [currentPageUrl, setCurrnentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
 
   useEffect(() => {
     axios.get(currentPageUrl).then(res => {
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
     })
   }, [currentPageUrl])
